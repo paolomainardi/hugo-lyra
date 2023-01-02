d25f395ca01a8ed4d9542212714c36d6eb754b74
Paolo Mainardi <paolomainardi@gmail.com>
2023-01-02 17:48:26 +0100

    refs #14:  Browser client search implementation (#15)
    * feat(browser): add a library to build client side search page

---
+++ tests/browser/hugo-lyra-browser.test.ts
@@ -0,0 +1,86 @@
+import t from "tap";
+import { HugoLyra, HugoLyraBrowserOptions } from "../../src/browser/hugo-lyra-browser";
+import { create, insert, search } from "@lyrasearch/lyra";
+
+t.test("Hugo lyra browser implementation", t => {
+  t.plan(4);
+
+  t.test("Test with empty query parameter", t => {
+    t.plan(1);
+
+    const db = create({
+      schema: {
+        quote: "string",
+        author: "string",
+      },
+    });
+
+    const opts: HugoLyraBrowserOptions = {
+      queryString: "",
+    };
+    /* eslint-disable  @typescript-eslint/no-explicit-any */
+    const res = HugoLyra().search(db as any, opts);
+    t.same(undefined, res);
+  });
+
+  t.test("should return undefined when param is not part of querystring", t => {
+    t.plan(1);
+
+    const db = create({
+      schema: {
+        quote: "string",
+        author: "string",
+      },
+    });
+
+    const opts: HugoLyraBrowserOptions = {
+      queryString: "?q=foo",
+      param: "bar",
+    };
+    /* eslint-disable  @typescript-eslint/no-explicit-any */
+    const res = HugoLyra().search(db as any, opts);
+    t.same(undefined, res);
+  });
+
+  t.test("should return undefined when querystring and param are empty", t => {
+    t.plan(1);
+
+    const db = create({
+      schema: {
+        quote: "string",
+        author: "string",
+      },
+    });
+
+    const opts: HugoLyraBrowserOptions = {
+      queryString: "",
+      param: "",
+    };
+    const res = HugoLyra().search(db as any, opts);
+    t.same(undefined, res);
+  });
+
+  t.test("should return a result when query string is valid", t => {
+    t.plan(1);
+
+    const db = create({
+      schema: {
+        quote: "string",
+        author: "string",
+      },
+    });
+    insert(db, {
+      quote: "I am a great programmer",
+      author: "Bill Gates",
+    });
+    const opts: HugoLyraBrowserOptions = {
+      queryString: "?q=bill",
+    };
+    /* eslint-disable  @typescript-eslint/no-explicit-any */
+    const res = HugoLyra().search(db as any, opts);
+    const qp1 = search(db, {
+      term: "bill",
+    });
+    t.same(qp1.hits, res?.search.hits);
+  });
+});
