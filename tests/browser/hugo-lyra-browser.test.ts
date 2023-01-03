f0cc4def9e48dcb9b51c8c293619bf905b1ebe5d
Paolo Mainardi <paolomainardi@gmail.com>
2023-01-03 18:00:03 +0000

    feat: refactor calculateUri and add tests for browser

--- tests/browser/hugo-lyra-browser.test.ts
+++ tests/browser/hugo-lyra-browser.test.ts
@@ -1,6 +1,56 @@
 import t from "tap";
 import { HugoLyra, HugoLyraBrowserOptions } from "../../src/browser/hugo-lyra-browser";
-import { create, insert, search } from "@lyrasearch/lyra";
+import { create, insert, Lyra, search } from "@lyrasearch/lyra";
+import { exportInstance } from "@lyrasearch/plugin-data-persistence";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function generateTestDBInstance(): Lyra<any> {
+  const db = create({
+    schema: {
+      quote: "string",
+      author: "string",
+    },
+  });
+
+  insert(db, {
+    quote: "I am a great programmer",
+    author: "Bill Gates",
+  });
+
+  insert(db, {
+    quote: "Be yourself; everyone else is already taken.",
+    author: "Oscar Wilde",
+  });
+
+  insert(db, {
+    quote: "I have not failed. I've just found 10,000 ways that won't work.",
+    author: "Thomas A. Edison",
+  });
+
+  insert(db, {
+    quote: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs",
+  });
+
+  return db;
+}
+
+t.test("Test bootstrap", t => {
+  t.plan(1);
+  t.test("bootstrap return a lyra database", t => {
+    t.plan(1);
+    const db = generateTestDBInstance();
+    const serializedData = exportInstance(db, "json");
+    const dbRestored = HugoLyra().bootstrap(serializedData);
+    const qp1 = search(db, {
+      term: "way",
+    });
+    const qp2 = search(dbRestored, {
+      term: "way",
+    });
+    t.same(qp1.hits, qp2.hits);
+  });
+});
 
 t.test("Test search", t => {
   t.plan(4);
